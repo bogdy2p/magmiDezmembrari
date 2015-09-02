18adb99a35e07aacd63a5348fd6cd91213b0e11b
Popa <bogdy2p@yahoo.com>
2015-09-02 10:24:40 +0300

    OOP-ed

--- index.php
+++ index.php
@@ -2,125 +2,197 @@
 
 require_once('parsecsv.lib.php');
 
-$csv = new parseCSV();
-$csv->auto('input.csv');
+ini_set('display_errors', 1);
 
-$csv_column_titles = $csv->titles;
+//$csv = new parseCSV();
+//$csv->auto('input.csv');
 
-$csv_data = $csv->data;
-//Instantiate the OutPut Array
-$output_data = array();
-
-
-$columns_to_be_added = array(
+//$output_data = array();
+$column_names_to_be_added_array = array(
   'YEAR', 'CCM', 'POWER', 'HORSEPOWER', 'ENGCODE'
 );
 
-foreach ($columns_to_be_added as $column_name) {
-  $csv->titles[] = $column_name;
+class PbcMagmi {
+
+  public $columns_to_be_added = array(
+    'YEAR',
+    'CCM',
+    'POWER',
+    'HORSEPOWER',
+    'ENGCODE'
+  );
+
+  public function __construct($inputFilename) {
+    $this->inputFileName = $inputFilename;
+    $this->csv = new parseCSV();
+    $this->csv->auto($this->inputFileName);
+    $this->column_titles = $this->csv->titles;
+    $this->csv_data = $this->csv->data;
+    $this->output_data = array();
+  }
+
+  function addColumnsToTitles($column_names_to_be_added_array) {
+    foreach ($column_names_to_be_added_array as $column_name) {
+      $this->csv->titles[] = $column_name;
+    }
+    return $this;
+  }
+
+  function expandExplanaitionField($data_array) {
+    foreach ($data_array as $key => $value) {
+
+      $yearmatch = preg_match("(((19|20)\d{2})|((19|20)\d{2}\.))", $value['EXPL'], $yearmatches);
+      if ($yearmatch) {
+        $year = substr($yearmatches[0], 0, 4);
+      }
+      else {
+        $year = "NULL";
+      }
+      $capacitymatch = preg_match("([0-9]{3,5}CC)", $value['EXPL'], $capacitymatches);
+      if ($capacitymatch) {
+        $capacity = $capacitymatches[0];
+      }
+      else {
+        $capacity = "NULL";
+      }
+      $powermatch = preg_match("([0-9]{1,4}KW)", $value['EXPL'], $powermatches);
+      if ($powermatch) {
+        $power = $powermatches[0];
+      }
+      else {
+        $power = "NULL";
+      }
+      $horsepowermatch = preg_match("([0-9]{1,4}CP)", $value['EXPL'], $horsepowermatches);
+      if ($horsepowermatch) {
+        $horsepower = $horsepowermatches[0];
+      }
+      else {
+        $horsepower = "NULL";
+      }
+      $enginecodematch = preg_match("([A-Z0-9]{12,25})", $value['EXPL'], $enginecodematches);
+      if ($enginecodematch) {
+        $enginecode = $enginecodematches[0];
+      }
+      else {
+        $enginecode = "NULL";
+      }
+
+      $value['YEAR'] = $year;
+      $value['CAPACITY'] = $capacity;
+      $value['POWER'] = $power;
+      $value['HORSEPOWER'] = $horsepower;
+      $value['ENGCODE'] = $enginecode;
+
+      $output_csv_data['sku'] = $value['ID'];
+      $output_csv_data['name'] = $value['DENUMIRE'];
+      $output_csv_data['price'] = $value['PRET_LISTA'];
+      $output_csv_data['qty'] = $value['STOC_CURENT'];
+      $output_csv_data['year'] = $value['YEAR'];
+      $output_csv_data['capacity'] = $value['CAPACITY'];
+      $output_csv_data['power'] = $value['POWER'];
+      $output_csv_data['horsepower'] = $value['HORSEPOWER'];
+      $output_csv_data['engcode'] = $value['ENGCODE'];
+
+      $this->output_data[] = $output_csv_data;
+    }
+    return $this;
+  }
+
+//End of expandExplanaitionField
+
+  function saveTheOutput($filename, $output_data) {
+
+    if ($this->output_data[0] != NULL) {
+      $outputCSV = new parseCSV();
+      $newcolumns = array_keys($this->output_data[0]);
+      $outputCSV->titles = $newcolumns;
+      $outputCSV->data = $this->output_data;
+      $outputCSV->save($filename, $this->output_data);
+//    console.log("wasdasd");
+      return $this;
+    }
+  }
+
 }
 
 
+$asd = new PbcMagmi('input.csv');
+$asd->addColumnsToTitles($asd->columns_to_be_added);
+$output_data = $asd->expandExplanaitionField($asd->csv->data);
+$asd->saveTheOutput('asdoutputfilename.csv', $output_data);
 
-foreach ($csv->data as $key => $value) {
-
-
-  $yearmatch = preg_match("(((19|20)\d{2})|((19|20)\d{2}\.))", $value['EXPL'], $yearmatches);
-  if ($yearmatch) {
-    $year = substr($yearmatches[0], 0, 4);
-  }
-  else {
-    $year = "NULL";
-  }
-  $capacitymatch = preg_match("([0-9]{3,5}CC)", $value['EXPL'], $capacitymatches);
-  if ($capacitymatch) {
-    $capacity = $capacitymatches[0];
-  }
-  else {
-    $capacity = "NULL";
-  }
-  $powermatch = preg_match("([0-9]{1,4}KW)", $value['EXPL'], $powermatches);
-  if ($powermatch) {
-    $power = $powermatches[0];
-  }
-  else {
-    $power = "NULL";
-  }
-  $horsepowermatch = preg_match("([0-9]{1,4}CP)", $value['EXPL'], $horsepowermatches);
-  if ($horsepowermatch) {
-    $horsepower = $horsepowermatches[0];
-  }
-  else {
-    $horsepower = "NULL";
-  }
-  $enginecodematch = preg_match("([A-Z0-9]{12,25})", $value['EXPL'], $enginecodematches);
-  if ($enginecodematch) {
-    $enginecode = $enginecodematches[0];
-  }
-  else {
-    $enginecode = "NULL";
-  }
-
-  $value['YEAR'] = $year;
-  $value['CAPACITY'] = $capacity;
-  $value['POWER'] = $power;
-  $value['HORSEPOWER'] = $horsepower;
-  $value['ENGCODE'] = $enginecode;
-
-  $output_csv_data['sku'] = $value['ID'];
-  $output_csv_data['name'] = $value['DENUMIRE'];
-  $output_csv_data['price'] = $value['PRET_LISTA'];
-  $output_csv_data['qty'] = $value['STOC_CURENT'];
-  $output_csv_data['year'] = $value['YEAR'];
-  $output_csv_data['capacity'] = $value['CAPACITY'];
-  $output_csv_data['power'] = $value['POWER'];
-  $output_csv_data['horsepower'] = $value['HORSEPOWER'];
-  $output_csv_data['engcode'] = $value['ENGCODE'];
-
-  $output_data[] = $output_csv_data;
-}
-
-
-
-
-//    [0] => ID
-//    [1] => ID_UM_MINIMA
-//    [2] => COD_INTERN
-//    [3] => DENUMIRE
-//    [4] => PRET_LISTA
-//    [5] => PRET_VANZ_MAG
-//    [6] => GRAMAJ_UNITATEA_MINIMA
-//    [7] => ORDINE_AFISARE
-//    [8] => XDISC_PROMO
-//    [9] => XTVA
-//    [10] => XADAOS
-//    [11] => PRODUS_ACTIV
-//    [12] => ACOPERIRE
-//    [13] => COD_BARE
-//    [14] => COMENT
-//    [15] => EXPL
-//    [16] => PRET_LISTA_EURO
-//    [17] => PAGINA_CATALOG
-//    [18] => LINK_DISK
-//    [19] => FIELD_1
-//    [20] => FIELD_2
-//    [21] => STOC_COMANDAT_CLN
-//    [22] => STOC_COMANDAT_FUR
-//    [23] => STOC_CURENT
-//    [24] => STOC_REZERVAT
-//    [25] => STOC_ASIGURAT
-//    [26] => PARENT_ID
-//    [27] => SOURCE_ID
-# then we output the file to the browser as a downloadable file...
-//$csv->save('output.csv', $output_csv_data);
-
-
-//var_dump($output_data);
-
-$outputCSV = new parseCSV();
-$newcolumns = array_keys($output_csv_data);
-$outputCSV->titles = $newcolumns;
-$outputCSV->data = $output_data;
-$outputCSV->save('outputForMagmi.csv', $output_data);
-
+//echo "<pre>";
+//print_r($csv->titles);
+//foreach ($column_names_to_be_added_array as $column_name) {
+//  $csv->titles[] = $column_name;
+//}
+//print_r($csv->titles);
+//
+//
+//foreach ($csv->data as $key => $value) {
+////
+////
+////  $yearmatch = preg_match("(((19|20)\d{2})|((19|20)\d{2}\.))", $value['EXPL'], $yearmatches);
+////  if ($yearmatch) {
+////    $year = substr($yearmatches[0], 0, 4);
+////  }
+////  else {
+////    $year = "NULL";
+////  }
+////  $capacitymatch = preg_match("([0-9]{3,5}CC)", $value['EXPL'], $capacitymatches);
+////  if ($capacitymatch) {
+////    $capacity = $capacitymatches[0];
+////  }
+////  else {
+////    $capacity = "NULL";
+////  }
+////  $powermatch = preg_match("([0-9]{1,4}KW)", $value['EXPL'], $powermatches);
+////  if ($powermatch) {
+////    $power = $powermatches[0];
+////  }
+////  else {
+////    $power = "NULL";
+////  }
+////  $horsepowermatch = preg_match("([0-9]{1,4}CP)", $value['EXPL'], $horsepowermatches);
+////  if ($horsepowermatch) {
+////    $horsepower = $horsepowermatches[0];
+////  }
+////  else {
+////    $horsepower = "NULL";
+////  }
+////  $enginecodematch = preg_match("([A-Z0-9]{12,25})", $value['EXPL'], $enginecodematches);
+////  if ($enginecodematch) {
+////    $enginecode = $enginecodematches[0];
+////  }
+////  else {
+////    $enginecode = "NULL";
+////  }
+////
+////  $value['YEAR'] = $year;
+////  $value['CAPACITY'] = $capacity;
+////  $value['POWER'] = $power;
+////  $value['HORSEPOWER'] = $horsepower;
+////  $value['ENGCODE'] = $enginecode;
+////
+////  $output_csv_data['sku'] = $value['ID'];
+////  $output_csv_data['name'] = $value['DENUMIRE'];
+////  $output_csv_data['price'] = $value['PRET_LISTA'];
+////  $output_csv_data['qty'] = $value['STOC_CURENT'];
+////  $output_csv_data['year'] = $value['YEAR'];
+////  $output_csv_data['capacity'] = $value['CAPACITY'];
+////  $output_csv_data['power'] = $value['POWER'];
+////  $output_csv_data['horsepower'] = $value['HORSEPOWER'];
+////  $output_csv_data['engcode'] = $value['ENGCODE'];
+////
+////  $output_data[] = $output_csv_data;
+//}
+//
+///**
+// * Output the data into the "outputForMagmi.csv" file.
+// */
+//$outputCSV = new parseCSV();
+//$newcolumns = array_keys($output_csv_data);
+//$outputCSV->titles = $newcolumns;
+//$outputCSV->data = $output_data;
+//$outputCSV->save('outputForMagmi.csv', $output_data);
 ?>
